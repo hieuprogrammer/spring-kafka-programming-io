41b4f8310028e5c612b19610dda3ab2b91449a20
Hieu LE MINH <hieu.leminh@positivethinking.tech>
2023-05-17 09:47:28 +0700

    Implemented storing consumed data into a database (H2/Postgres/MySQL).

--- src/main/java/ai/tech/domain/model/Todo.java
+++ src/main/java/ai/tech/domain/model/Todo.java
@@ -1,10 +1,23 @@
 package ai.tech.domain.model;
 
 import ai.tech.domain.enums.Status;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
-@Data
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
 public class Todo {
+    @Id @GeneratedValue(strategy = GenerationType.UUID) private UUID id;
     private String todo;
     private Status status;
 }
--- src/main/java/ai/tech/service/infrastructure/consumer/SpringKafkaConsumer.java
+++ src/main/java/ai/tech/service/infrastructure/consumer/SpringKafkaConsumer.java
@@ -1,21 +1,28 @@
 package ai.tech.service.infrastructure.consumer;
 
 import ai.tech.domain.model.Todo;
+import ai.tech.repository.TodoRepository;
 import ai.tech.service.infrastructure.util.KafkaConstants;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class SpringKafkaConsumer {
+    private final TodoRepository todoRepository;
+
     @KafkaListener(topics = KafkaConstants.SPRING_KAFKA_TOPIC_NAME, groupId = "spring-kafka")
     public void consumeFromSpringKafkaTopic(final String message) {
         log.info("Message: \"{}\", received from topic: \"{}\"", message, KafkaConstants.SPRING_KAFKA_TOPIC_NAME);
     }
 
     @KafkaListener(topics = KafkaConstants.SPRING_KAFKA_TODO_TOPIC_NAME, groupId = "spring-kafka-todo")
-    public void consumeFromSpringKafkaTodoTopic(final Todo message) {
-        log.info("Message: \"{}\", received from topic: \"{}\"", message.toString(), KafkaConstants.SPRING_KAFKA_TODO_TOPIC_NAME);
+    public void consumeFromSpringKafkaTodoTopic(final Todo todoEventMessage) {
+        log.info("Message: \"{}\", received from topic: \"{}\"", todoEventMessage.toString(),
+                KafkaConstants.SPRING_KAFKA_TODO_TOPIC_NAME);
+        todoRepository.save(todoEventMessage);
     }
 }
